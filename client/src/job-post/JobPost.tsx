db6f3e3644feb581c68acd5e63587ad141b12c22
felixoder <debayanghosh408@gmail.com>
2024-07-12 10:36:29 +0530

    solved the jobpost problem

--- client/src/job-post/JobPost.tsx
+++ client/src/job-post/JobPost.tsx
@@ -1,69 +1,65 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
+import  { useState, useEffect, ChangeEvent } from 'react';
+import { GetCountries, GetState, GetCity } from 'react-country-state-city';
 import Select from 'react-select';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateFailure, updateStart, updateSuccess } from '../redux/user/userSlice';
 import toast, { Toaster } from 'react-hot-toast';
-import MaxWidthWrapper from '../components/MaxWidthWrapper';
-import { RootState } from '../redux/store';
-import { GetCountries, GetState, GetCity } from 'react-country-state-city';
 import { useNavigate } from 'react-router-dom';
 import { Oval } from 'react-loader-spinner';
+import MaxWidthWrapper from '../components/MaxWidthWrapper';
+import { RootState } from '../redux/store';
 
 interface FormData {
-  fullname: string;
+  title: string;
+  post: string;
+  company: string;
   address: string;
+  glassdoor: boolean;
+  logo: string;
+  author: string;
+  type: string;
   skills: string;
-  resume: string;
-  portfolio: string;
-  position: string;
-  profile_image: string;
-  bio: string;
-  description: string;
+  salary: string;
+  experience: string;
+  glassdoortext?: string; // Optional property for glassdoor answer
 }
 
 interface User {
-  fullname: string;
-  description: string;
-  bio: string;
-  address: string;
-  portfolio: string;
-  skills: string;
-  resume: string;
-  profile_image: string;
-  position: string;
   username: string;
 }
 
-const EditJob: React.FC = () => {
+
+const JobPost = () => {
   const [countryId, setCountryId] = useState<string | null>(null);
   const [stateId, setStateId] = useState<string | null>(null);
   const [, setCityId] = useState<string | null>(null);
+  const currentUser = useSelector((state: RootState) => state.user.currentUser) as User | null;
+
   const [formData, setFormData] = useState<FormData>({
-    fullname: '',
+    title: '',
+    post: '',
+    logo: '',
     address: '',
+    glassdoor: false,
+    company: '',
+    author: currentUser?.username || '',
+    type: 'remote',
     skills: '',
-    resume: '',
-    portfolio: '',
-    position: '',
-    profile_image: '',
-    bio: '',
-    description: '',
+    salary: '',
+    experience: '',
   });
+console.log(formData)
   const navigate = useNavigate();
   const [countriesList, setCountriesList] = useState<any[]>([]);
   const [stateList, setStateList] = useState<any[]>([]);
   const [cityList, setCityList] = useState<any[]>([]);
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
-  const currentUser = useSelector((state: RootState) => state.user.currentUser) as User | null;
 
   useEffect(() => {
-    // Fetch countries list on component mount
     GetCountries().then((result: React.SetStateAction<any[]>) => setCountriesList(result));
   }, []);
 
   const handleCountryChange = (selectedOption: any) => {
-    // Handle country change and fetch states
     setCountryId(selectedOption.id);
     setStateId(null);
     setCityId(null);
@@ -73,7 +69,6 @@ const EditJob: React.FC = () => {
   };
 
   const handleStateChange = (selectedOption: any) => {
-    // Handle state change and fetch cities
     setStateId(selectedOption.id);
     setCityId(null);
     setFormData({ ...formData, address: `${formData.address}, ${selectedOption.name}` });
@@ -82,86 +77,107 @@ const EditJob: React.FC = () => {
   };
 
   const handleCityChange = (selectedOption: any) => {
-    // Handle city change
     setCityId(selectedOption.id);
     setFormData({ ...formData, address: `${formData.address}, ${selectedOption.name}` });
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // Handle form input changes
+  const handleGlassdoorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const glassdoor = e.target.value === 'yes';
+    setFormData({ ...formData, glassdoor, glassdoortext: glassdoor ? formData.glassdoortext : '' });
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
 
-    try {
-      dispatch(updateStart());
-      setLoading(true);
+    if (!formData.title || !formData.post || !formData.company || !formData.address || !formData.author || !formData.type) {
+      toast.error('Please fill all the fields');
+      return;
+    }
 
-      const res = await fetch(`https://jobera.onrender.com/api/edit-profile/${currentUser?.username}`, {
-        method: 'PUT',
+    try {
+      setLoading(true); // Set loading state to true
+      const res = await fetch('https://jobera.onrender.com/api/post-jobs', {
+        method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
 
       const data = await res.json();
-
-      if (!res.ok) {
-        throw new Error(data.message || 'Failed to update profile.');
-      }
-
-      dispatch(updateSuccess(data));
-      toast.success('Profile updated successfully.');
+      toast.success('Job posted successfully!');
+      console.log(data.message)
+      navigate('/your-jobs');  // Redirect to jobs page
     } catch (error) {
-      console.error('Error updating profile:', error);
-      toast.error('Failed to update profile. Please try again later.');
-      dispatch(updateFailure( 'Failed to update profile.'));
+      toast.error('Sorry, try again later!');
     } finally {
-      setLoading(false);
-      navigate(`/profile/${currentUser?.username}`);
+      setLoading(false); // Set loading state to false
     }
   };
 
   return (
     <MaxWidthWrapper>
       <Toaster />
-      <div className="flex flex-col lg:grid grid-cols-1">
-        <MaxWidthWrapper className="flex mx-auto">
-          <div className="w-full max-w-md mx-auto mt-10 mb-10">
-            <h1 className="text-center font-bold text-xl">Edit Profile</h1>
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+      <div className="w-full mx-auto mt-10 mb-10 container">
+        <h1 className="text-center font-bold text-xl mb-6">Post<span className="text-green-600 font-bold underline">Job</span></h1>
+        <form className="bg-white shadow-md rounded-lg px-4 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col md:grid grid-cols-2 gap-10">
+            <div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Full Name</label>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="post">Job Post</label>
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="fullname"
-                  defaultValue={currentUser?.fullname || ''}
+                  id="post"
                   type="text"
-                  placeholder="Enter Your Full Name"
+                  placeholder="Enter the Job Post (eg, SDE)"
+                  required
                   onChange={handleChange}
                 />
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Position</label>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Job Title</label>
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="position"
+                  id="title"
                   type="text"
-                  placeholder="Enter Your Position"
+                  placeholder="Enter the Job Title"
+                  required
                   onChange={handleChange}
-                  defaultValue={currentUser?.position || ''}
                 />
               </div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Profile Image URL</label>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">Company Name</label>
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="profile_image"
+                  id="company"
                   type="text"
-                  placeholder="Enter Your Profile Image URL"
+                  placeholder="Enter The company Name"
+                  required
                   onChange={handleChange}
-                  defaultValue={currentUser?.profile_image || ''}
+                />
+              </div>
+              <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="logo">Company Logo</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="logo"
+                  type="text"
+                  placeholder="Enter the logo of the company"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="skills">Skills Required</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="skills"
+                  type="text"
+                  placeholder="Enter The Skills required"
+                  required
+                  onChange={handleChange}
                 />
               </div>
               <div className="mb-4">
@@ -196,68 +212,81 @@ const EditJob: React.FC = () => {
                   isDisabled={!stateId}
                 />
               </div>
+            </div>
+            <div>
               <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Portfolio Link</label>
-                <input
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="glassdoor">Glassdoor</label>
+                <select
+                  id="glassdoor"
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="portfolio"
-                  type="text"
-                  placeholder="Enter Your Portfolio Link"
-                  onChange={handleChange}
-                  defaultValue={currentUser?.portfolio || ''}
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Skills</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="skills"
-                  type="text"
-                  placeholder="Enter Your Skills"
-                  onChange={handleChange}
-                  defaultValue={currentUser?.skills || ''}
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Resume Link</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="resume"
-                  type="text"
-                  placeholder="Enter Your Resume Link"
-                  onChange={handleChange}
-                  defaultValue={currentUser?.resume || ''}
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Your Bio Here</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="bio"
-                  type="text"
-                  placeholder="Enter Your Bio Here"
-                  onChange={handleChange}
-                  defaultValue={currentUser?.bio || ''}
-                />
-              </div>
-              <div className="mb-6 text-center">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="submit"
+                  onChange={handleGlassdoorChange}
                 >
-                  {loading ? (
-                          <Oval color="#fff" height={24} width={24} />
-                  ) : (
-                    'Update Profile'
-                  )}
-                </button>
+                  <option value="">Select</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
               </div>
-            </form>
+              {formData.glassdoor && (
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="glassdoortext">Glassdoor Answer</label>
+                  <textarea
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="glassdoortext"
+                    placeholder="Enter Glassdoor Answer"
+                    onChange={handleChange}
+                    value={formData.glassdoortext}
+                  />
+                </div>
+              )}
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="salary">Salary</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="salary"
+                  type="text"
+                  placeholder="Enter the Salary"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="experience">Experience Required</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="experience"
+                  type="text"
+                  placeholder="Enter the Experience"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="type">Job Type</label>
+                <select
+                  id="type"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  onChange={handleChange}
+                  value={formData.type}
+                >
+                  <option value="remote">Remote</option>
+                  <option value="on-site">On-Site</option>
+                  <option value="hybrid">Hybrid</option>
+                </select>
+              </div>
+            </div>
           </div>
-        </MaxWidthWrapper>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              {loading ? <Oval height={20} width={20} color="white" ariaLabel="loading" /> : 'Post Job'}
+            </button>
+          </div>
+        </form>
       </div>
     </MaxWidthWrapper>
   );
 };
 
-export default EditJob;
+export default JobPost;
