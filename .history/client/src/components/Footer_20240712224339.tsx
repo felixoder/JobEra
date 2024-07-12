3deb074fd55aac9123a89df62207a1944a159e0b
felixoder <debayanghosh408@gmail.com>
2024-07-12 22:44:17 +0530

    footer is changed

--- .history/client/src/components/Footer_20240712224339.tsx
+++ .history/client/src/components/Footer_20240712224339.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
 <footer className="bg-slate-100 rounded-lg shadow m-4 ">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-black-500 sm:text-center">©  {new Date().getFullYear()} {"  "}<a href="https://flowbite.com/" className="hover:underline">JobEra™</a>. All Rights Reserved.
+      <span className="text-sm text-black-500 sm:text-center">©  {new Date().getFullYear()}<a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
